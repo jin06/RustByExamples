1b830bc9a069f90c99dc7c105562cf21867995a2
jinlong <jlonmyway@gmail.com>
2024-09-09 14:38:16 +0800

    hello world example

---
+++ examples/hello_world/main.rs
@@ -0,0 +1,13 @@
+// When this program is executed, it outputs the text "Hello World!" to the console. 
+// This is a standard example used in many programming languages to demonstrate the basic syntax and functionality.
+
+// This defines the main function, which is the entry point of every Rust program. When you run the program, execution starts here.
+fn main() {
+    // println!: This is a macro in Rust used to print text to the console. In this case, it prints "Hello World!". 
+    // The ! indicates that println is a macro, not a regular function. 
+    // Macros in Rust can generate code at compile time and are commonly used for tasks like formatting text or handling patterns that would otherwise require repetitive code.
+
+
+    // "Hello World!": This is a string literal. Rust uses double quotes for string literals, and this particular string will be printed to the console.
+    println!("Hello World!")  
+}
